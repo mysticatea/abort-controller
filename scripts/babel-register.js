1a8cba813f3cb8c37212c14456d7d41974fa821d
Toru Nagashima <star.ctor@gmail.com>
2018-03-25 17:25:59 +0900

    Fix: node 4 problem in tests

--- scripts/babel-register.js
+++ scripts/babel-register.js
@@ -1,9 +1,11 @@
 require("babel-register")({
     babelrc: false,
-    extensions: [".mjs"],
-    ignore: false,
+    extensions: [".mjs", ".js"],
+    ignore: (file) => {
+        return file.indexOf("/node_modules/") !== -1 && file.indexOf("/simple-spy/") === -1
+    },
     presets: [
-        ["env", { "targets": { "node": "4" } }]
+        ["env", { "targets": { "node": "4.0.0" } }]
     ],
     sourceMaps: "inline",
 })
