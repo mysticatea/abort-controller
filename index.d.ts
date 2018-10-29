a998478bc3a8888849033db90c40266f7a9a9df8
FrogTheFrog <warliukz@gmail.com>
2018-10-29 19:08:24 +0200

    remove module brackets

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,26 @@
-declare module "abort-controller" {  
-    import { Event, EventTarget } from "event-target-shim";
-    
-    export class AbortSignal extends EventTarget {
-        private constructor();
-    
-        /**
-         * Returns true if this AbortSignal's AbortController has signaled to abort, and false
-         * otherwise.
-         */
-        readonly aborted: boolean;
-        onabort: ((this: this, ev: Event) => any) | null;
-    }
-    
-    export class AbortController {
-        /**
-         * Returns the AbortSignal object associated with this object.
-         */
-        readonly signal: AbortSignal;
-    
-        /**
-         * Invoking this method will set this object's AbortSignal's aborted flag and
-         * signal to any observers that the associated activity is to be aborted.
-         */
-        abort(): void;
-    }
-    export default AbortController;
+import { Event, EventTarget } from "event-target-shim";
+
+export class AbortSignal extends EventTarget {
+    private constructor();
+
+    /**
+     * Returns true if this AbortSignal's AbortController has signaled to abort, and false
+     * otherwise.
+     */
+    readonly aborted: boolean;
+    onabort: ((this: this, ev: Event) => any) | null;
 }
+
+export class AbortController {
+    /**
+     * Returns the AbortSignal object associated with this object.
+     */
+    readonly signal: AbortSignal;
+
+    /**
+     * Invoking this method will set this object's AbortSignal's aborted flag and
+     * signal to any observers that the associated activity is to be aborted.
+     */
+    abort(): void;
+}
+export default AbortController;
