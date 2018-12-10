dcc250622c88325a75f856aa2178c88a17695851
Lukas Senionis <warliukz@gmail.com>
2018-12-10 12:28:08 +0200

    💥 add TypeScript definitions (fixes #6)(#11)

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+import { 
+    Event, 
+    EventTarget, 
+    AddEventListenerOptions, 
+    EventListenerOptions,
+    EventTargetListener
+ } from "event-target-shim";
+
+export as namespace AbortControllerShim;
+
+export type AbortSignalListener = (this: AbortSignal, ev: any) => any;
+
+export class AbortSignal extends EventTarget {
+    private constructor();
+
+    /**
+     * Returns true if this AbortSignal's AbortController has signaled to abort, and false
+     * otherwise.
+     */
+    readonly aborted: boolean;
+    onabort: AbortSignalListener | null;
+    addEventListener(type: "abort", listener: AbortSignalListener, options?: boolean | AddEventListenerOptions): void;
+    addEventListener(type: string, listener: EventTargetListener | null, options?: boolean | AddEventListenerOptions): void;
+    removeEventListener(type: "abort", listener: AbortSignalListener, options?: boolean | EventListenerOptions): void;
+    removeEventListener(type: string, listener: EventTargetListener | null, options?: boolean | EventListenerOptions): void;
+}
+
+export class AbortController {
+    /**
+     * Returns the AbortSignal object associated with this object.
+     */
+    readonly signal: AbortSignal;
+
+    /**
+     * Invoking this method will set this object's AbortSignal's aborted flag and
+     * signal to any observers that the associated activity is to be aborted.
+     */
+    abort(): void;
+}
+export default AbortController;
---
+++ test/types.ts
@@ -0,0 +1,30 @@
+import { AbortController as AbortControllerShim, AbortSignal as AbortSignalShim } from "..";
+
+function signalCallback(as: AbortSignal) { }
+function signalShimCallback(as: AbortSignalShim) { }
+function controllerCallback(ac: AbortController) { }
+function controllerShimCallback(ac: AbortControllerShim) { }
+function abortCallback(this: AbortSignal, ev: Event) { }
+
+const controller = new AbortController();
+const controllerShim = new AbortControllerShim();
+const signal = controller.signal;
+const signalShim = controllerShim.signal;
+
+signalCallback(signal);
+signalCallback(signalShim);
+
+signalShimCallback(signalShim);
+
+controllerCallback(controller);
+controllerCallback(controllerShim);
+
+controllerShimCallback(controllerShim);
+
+if (signalShim.aborted === false) {
+    signalShim.addEventListener("abort", abortCallback);
+    signalShim.removeEventListener("abort", abortCallback);
+    signalShim.onabort = null;
+    signalShim.onabort = abortCallback;
+}
+controllerShim.abort();
